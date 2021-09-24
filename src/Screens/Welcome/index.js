4659ddafc3fcc36ddf6724334f4ceb7c34a94196
ShaheerAliKhan <shaheer.khan@m3tech.com.pk>
2021-09-24 13:58:30 +0500

    Initial Commit

---
+++ src/Screens/Welcome/index.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import {
+    RTCPeerConnection,
+    RTCIceCandidate,
+    RTCSessionDescription,
+    RTCView,
+    MediaStream,
+    MediaStreamTrack,
+    mediaDevices,
+    registerGlobals
+} from 'react-native-webrtc';
+
+
+export default class Welcome extends Component {
+    componentDidMount() {
+        const configuration = { "iceServers": [{ "url": "stun:stun.l.google.com:19302" }] };
+        const pc = new RTCPeerConnection(configuration);
+        let isFront = true;
+
+        mediaDevices.enumerateDevices().then(sourceInfos => {
+            console.log(sourceInfos);
+            let videoSourceId;
+            for (let i = 0; i < sourceInfos.length; i++) {
+                const sourceInfo = sourceInfos[i];
+                if (sourceInfo.kind == "videoinput" && sourceInfo.facing == (isFront ? "front" : "environment")) {
+                    videoSourceId = sourceInfo.deviceId;
+                }
+            }
+            mediaDevices.getUserMedia({
+                audio: true,
+                video: {
+                    width: 640,
+                    height: 480,
+                    frameRate: 30,
+                    facingMode: (isFront ? "user" : "environment"),
+                    deviceId: videoSourceId
+                }
+            })
+                .then(stream => {
+                    // Got stream!
+                })
+                .catch(error => {
+                    // Log error
+                });
+        });
+
+        pc.createOffer().then(desc => {
+            pc.setLocalDescription(desc).then(() => {
+                // Send pc.localDescription to peer
+            });
+        });
+
+        pc.onicecandidate = function (event) {
+            // send event.candidate to peer
+        };
+    }
+
+    render() {
+        return (
+            <View>
+                {/* <RTCView streamURL={this.state.stream.toURL()} /> */}
+            </View>
+        )
+    }
+}
