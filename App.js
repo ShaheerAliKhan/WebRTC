5cabb0d21150129cca54b922e4b4531c4e74461a
ShaheerAliKhan <shaheer.khan@m3tech.com.pk>
2021-09-24 14:10:39 +0500

    Changes

--- App.js
+++ App.js
@@ -1,6 +1,26 @@
-import React from "react"
-import Routes from './src/Routes'
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-const App = () => <Routes />
+import LoginScreen from './screens/LoginScreen';
+import CallScreen from './screens/CallScreen';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
-export default App
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen name="Call" component={CallScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
